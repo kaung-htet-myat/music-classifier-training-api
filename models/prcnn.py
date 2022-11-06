0866538e0111cc70e63420cd2afe988ea9841eb0
kaung-htet-myat <kaunghm.09@gmail.com>
2022-11-06 14:28:17 +0630

    fix: larger timeframe for melspectrogram

--- models/prcnn.py
+++ models/prcnn.py
@@ -16,13 +16,13 @@ class PRCNN(nn.Module):
 
     super(PRCNN, self).__init__()
     self.feature_extractor = create_feature_extractor(backbone_model, return_nodes, suppress_diff_warning=True)
-    self.bn_1 = nn.BatchNorm2d(backbone_out_dims[0])
+    # self.bn_1 = nn.BatchNorm2d(backbone_out_dims[0])
     self.dropout_1 = nn.Dropout(0.2)
     self.avg_pool_1 = nn.AvgPool2d(kernel_size=(backbone_out_dims[1], backbone_out_dims[2]))
     self.maxpool_1 = nn.MaxPool2d((2, 1), stride=(2, 1))
     self.bi_rnn = nn.LSTM(int(input_dims[1]/2), hidden_dims_1, bidirectional=True, batch_first=True)
     self.dropout_2 = nn.Dropout(0.2)
-    self.linear_1 = nn.Linear(int(hidden_dims_1*2+backbone_out_dims[0]), hidden_dims_2)
+    self.linear_1 = nn.Linear(int(hidden_dims_1+backbone_out_dims[0]), hidden_dims_2)
     self.relu_1 = nn.ReLU()
     self.linear_2 = nn.Linear(hidden_dims_2, 10)
     self.softmax = nn.Softmax(dim=1)
@@ -40,7 +40,7 @@ class PRCNN(nn.Module):
     rnn_feat = torch.permute(rnn_feat, (0, 2, 1))
     _, h_n = self.bi_rnn(rnn_feat)
     rnn_feat = h_n[0]
-    rnn_feat = torch.concat([rnn_feat[0], rnn_feat[1]], dim=-1)
+    rnn_feat = rnn_feat[0] + rnn_feat[1]
     rnn_feat = self.dropout_2(rnn_feat)
 
     concat_feat = torch.concat([flattened, rnn_feat], dim=-1)
